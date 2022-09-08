b94824c5753a201cd1e2694c7ca294fae139a471
Mike Kerzhner <mikerzhner@gmail.com>
2022-09-08 13:25:01 +0200

    Add Nitro contracts, remove sample contract
    Also remove package-lock.json since yarn is used

--- hardhat.config.local.js
+++ hardhat.config.local.js
@@ -3,29 +3,33 @@ const fs = require("fs");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("my-deploy", "Deploys contract, get wallets, and outputs files", async (taskArgs, hre) => {
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+task(
+  "my-deploy",
+  "Deploys contract, get wallets, and outputs files",
+  async (taskArgs, hre) => {
+    // We get the contract to deploy
+    const NA = await hre.ethers.getContractFactory("NitroAdjudicator");
+    const na = await NA.deploy();
 
-  // Await deployment
-  await greeter.deployed();
+    // Await deployment
+    await na.deployed();
 
-  // Get address
-  const contractAddress = greeter.address;
+    // Get address
+    const contractAddress = na.address;
 
-  // Write file
-  fs.writeFileSync('./.contract', contractAddress);
+    // Write file
+    fs.writeFileSync("./.contract", contractAddress);
 
-  // Get generated signer wallets
-  const accounts = await hre.ethers.getSigners();
+    // Get generated signer wallets
+    const accounts = await hre.ethers.getSigners();
 
-  // Get the first wallet address
-  const walletAddress = accounts[0].address;
+    // Get the first wallet address
+    const walletAddress = accounts[0].address;
 
-  // Write file
-  fs.writeFileSync('./.wallet', walletAddress);
-});
+    // Write file
+    fs.writeFileSync("./.wallet", walletAddress);
+  },
+);
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -34,10 +38,10 @@ task("my-deploy", "Deploys contract, get wallets, and outputs files", async (tas
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.7.6",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
-  }
+  },
 };
