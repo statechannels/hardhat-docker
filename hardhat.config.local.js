375c12f324fe224b7b71c6833cbf35a057c0e908
geoknee <georgeknee@googlemail.com>
2022-09-13 13:14:17 +0100

    update bytecode

--- hardhat.config.local.js
+++ hardhat.config.local.js
@@ -1,12 +1,13 @@
 require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
-const { abi, bytecode } = require("./NitroAdjudicator.json");
+const { bytecode } = require("./NitroAdjudicator.json");
 
 task(
   "deploy",
   "Deploys contract and writes address to ./.contract",
   async (taskArgs, hre) => {
     // We get the contract to deploy
+    const abi = []; // stubbed out since it is unecessary here
     const NA = await hre.ethers.getContractFactory(abi, bytecode);
     const na = await NA.deploy();
 
