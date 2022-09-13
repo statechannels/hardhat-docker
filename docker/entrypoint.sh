9eb486d4455d33b41cc8fbb9af5957c10fda8161
geoknee <georgeknee@googlemail.com>
2022-09-13 10:33:08 +0100

    combine lines in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,10 +5,9 @@ cd /usr/src/app;
 
 # Start hardhat node as a background process
 yarn start:local &
-# Wait for hardhat node to initialize.
-npx wait-on localhost:8545
-# Deploy contracts
-yarn deploy:local;
+
+# Wait for hardhat node to initialize and then deploy contracts
+npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
 # The hardhat node process never completes
 # Waiting prevents the container from pausing
