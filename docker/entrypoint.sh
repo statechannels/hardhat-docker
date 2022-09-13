e9392624ea38944f343a3527ada34bfcfbdae545
geoknee <georgeknee@googlemail.com>
2022-09-13 10:17:38 +0100

    use wait-on instead of fixed wait duration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,8 @@ cd /usr/src/app;
 
 # Start hardhat node as a background process
 yarn start:local &
-# Wait for hardhat node to initialize. There doesn't seem to be a signal from hardhat node
-# to indicate initialization
-sleep 5;
+# Wait for hardhat node to initialize.
+npx wait-on localhost:8545
 # Deploy contracts
 yarn deploy:local;
 
